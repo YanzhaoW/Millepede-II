066b2ce4d8a0c3f387c6e1f6a823e04fc440699b
tomeichlersmith <eichl008@umn.edu>
2023-03-20 14:24:37 -0500

    update argument parser and use beta-version of print function
    this allows the code to be the same for python2 and python3 when we are
    printing messages and improves the CLI, making a help message
    automatically and documenting the different user options

--- tools/readMilleBinary.py
+++ tools/readMilleBinary.py
@@ -41,58 +41,69 @@
 #       local labels (array('i')) and derivatives (array('f') or array('d')).
 #
 # Tested with SL4, SL5, SL6
+#
+# Used containers to manually test Python 2.7.18 and Python 3.11.2
+#
+#   - Install [distrobox](https://distrobox.privatedns.org/)
+#     - source code: https://github.com/89luca89/distrobox
+#   - Construct a distrobox for the desired python version
+#       `distrobox create -i python:2 -n py2`
+#   - Enter it to do test runs
+#       `distrobox enter py2`
 
-import array, sys
+# in Python2.7, we can use the beta version of the print function
+#    imports from __future__ need to happen before any other code
+from __future__ import print_function
 
-# ############### read millepede binary file #################
-#  
-## Binary file type (C or Fortran)
-#Cfiles = 1  # Cfiles
-#Cfiles = 0 # Fortran files
-Cfiles = -1 # autodetect
-# 
+import sys
+import array
+
+PY3 = sys.version_info[0] == 3
+
+import argparse
+
+parser = argparse.ArgumentParser(description = 'read a mille binary file and print its data')
+parser.add_argument('--type', choices = ['c','fortran','autodetect'], default='autodetect',
+                    help='type of binary file that will be read')
+parser.add_argument('filename', help='binary file to read')
+parser.add_argument('-n','--num-records', type=int, default=10,
+                    help='number of records (i.e. tracks) to print to terminal')
+parser.add_argument('-s','--skip-records', type=int, default=0,
+                    help='number of records (tracks) to skip before starting to print')
+parser.add_argument('--min-val', type=float,
+                    help='minimum value to print derivatives')
+
+arg = parser.parse_args()
+
+fname = arg.filename
+mrec = arg.num_records
+skiprec = arg.skip_records
+minval = arg.min_val
+
 ## Integer format 
 intfmt = 'i'  # SL5, gcc-4
 #intfmt = 'l' # SL4, gcc-3
 #
-## Binary file name
-fname = "milleBinaryISN.dat"
-#
-## number of records (tracks) to show
-mrec = 10
-## number of records (track) to skip before 
-skiprec = 0
-## minimum value to print derivatives
-minval = None  # allows for NaNs
-#
 # ## C. Kleinwort - DESY ########################
 
-# ## use command line arguments ?
-narg = len(sys.argv)
-if narg > 1:
-  if narg < 3:
-    print " usage: readMilleBinary.py <file name> <number of records> [<number of records to skip> <minimum value to print derivative>]"
-    sys.exit(2)
-  else:
-    fname = sys.argv[1]
-    mrec = int(sys.argv[2])
-    if narg > 3:
-      skiprec = int(sys.argv[3])
-    if narg > 4:
-      minval = float(sys.argv[4])
-
-#print " input ", fname, mrec, skiprec
-
-# autodetect?
-if Cfiles < 0:
-  f = open(fname, "rb")
-  len2 = array.array(intfmt)
-  len2.fromfile(f, 2)
-  f.close()
-  Cfiles = 1 # C
-  if len2[0] == 4*(len2[1]+1): 
-    Cfiles = 0 # Fortran
-    print "Detected Fortran binary file"
+# the argument parser makes sure the user provides one of the elements 
+#   in the choices list, so if we get here, we can assume that arg.type
+#   is one of them.
+Cfiles = -1
+if arg.type == 'c' :
+    CFiles = 1
+elif arg.type == 'fortran' :
+    CFiles = 0
+else :
+    # need to auto-detect
+    f = open(fname, "rb")
+    len2 = array.array(intfmt)
+    len2.fromfile(f, 2)
+    f.close()
+    Cfiles = 1 # C
+    if len2[0] == 4*(len2[1]+1): 
+        Cfiles = 0 # Fortran
+        print("Detected Fortran binary file")
 
 # read file
 f = open(fname, "rb")
@@ -127,7 +138,7 @@ try:
         if (nrec <= skiprec):  # must be after last fromfile
             continue
 
-        print " === NR ", nrec, length[0] / 2
+        print(" === NR ", nrec, length[0] / 2)
 
         # no details, only header
         if (mrec < -1):
@@ -152,17 +163,17 @@ try:
                 jsp = jb
                 nsp = int(-glder[jb])
                 i += nsp - 1
-                print ' ### spec. ', nsp, inder[jsp + 1:i + 1], glder[jsp + 1:i + 1]
+                print(' ### spec. ', nsp, inder[jsp + 1:i + 1], glder[jsp + 1:i + 1])
                 continue
             while (i < nr) and (inder[i] != 0): i += 1
             i -= 1
             nh += 1
             if (jb < i):
 # measurement with global derivatives
-                print ' -g- meas. ', nh, inder[jb + 1], jb - ja - 1, i - jb, glder[ja], glder[jb]
+                print(' -g- meas. ', nh, inder[jb + 1], jb - ja - 1, i - jb, glder[ja], glder[jb])
             else:
 # measurement without global derivatives
-                print ' -l- meas. ', nh, inder[ja + 1], jb - ja - 1, i - jb, glder[ja], glder[jb]
+                print(' -l- meas. ', nh, inder[ja + 1], jb - ja - 1, i - jb, glder[ja], glder[jb])
             if (ja + 1 < jb):
                 lab = []
                 val = []
@@ -173,8 +184,8 @@ try:
                     elif abs(glder[k]) >= minval:
                         lab.append(inder[k])
                         val.append(glder[k])
-                print " local  ", lab
-                print " local  ", val
+                print(" local  ", lab)
+                print(" local  ", val)
             if (jb + 1 < i + 1):
                 lab = []
                 val = []
@@ -185,14 +196,14 @@ try:
                     elif abs(glder[k]) >= minval:
                         lab.append(inder[k])
                         val.append(glder[k])
-                print " global ", lab
-                print " global ", val
+                print(" global ", lab)
+                print(" global ", val)
 
 except EOFError:
-    print
+    print()
     if (nr > 0):
-        print " >>> error: end of file before end of record", nrec
+        print(" >>> error: end of file before end of record", nrec)
     else:
-        print " end of file after", nrec, "records"
+        print(" end of file after", nrec, "records")
 
 f.close()
